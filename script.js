8a2bb249c98387286325eb353d73d54e2ab020c0
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-01-24 18:47:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // write js code here if required
 const timer = document.getElementById('timer');
 
-const timerDisplay()=>{
+function timerDisplay()
 	let date = new Date();
+	let day = date.getDate();
+	let month = date.getMonth() + 1;
+	let year = date.getFullYear();
+	let hour = date.getHours();
+	let minutes = date.getMinutes();
+	let seconds = date.getSeconds();
+
+	return `${day}/${month}/${year},
+	${hour}:${minutes}:${seconds}`;
 }
 timer.textContent = timerDisplay();
+setInterval(()=>{
+	timer.textContent = timerDisplay();
+},1000);
